fbaa447ddcdd9e1dd51cb360c030f0bf8ca67285
MBello21 <magarbello@gmail.com>
2026-07-24 02:26:53 +0200

    feat(layout): add sidebar with lucide icons and NavLink active state

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,18 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
 import { Layout } from "./pages/Layout";
 import { NotFound } from "./pages/NotFound";
-
+import { Feature } from "./features/Feature";
 
 export const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path="/" element={<Layout />} errorElement={<NotFound />}>
-            </Route>
-        </>
-    )
-)
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Layout />} errorElement={<NotFound />}>
+        <Route path="/" element={<Feature />} />
+      </Route>
+    </>,
+  ),
+);
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,26 +1,25 @@
-
 interface Store {
-    msg: string
+  msg: string;
 }
 
 interface Action {
-    type: string
-    payload?: any
+  type: string;
+  payload?: unknown;
 }
 export const initialStore = () => {
-    return {
-        msg: 'Hola'
-    }
-}
+  return {
+    msg: "Hola",
+  };
+};
 
 export const storeReducer = (store: Store, action: Action): Store => {
-    switch (action.type) {
-        case 'set_msg':
-            return {
-                ...store,
-                msg: action.payload
-            }
-        default:
-            throw Error('Unknow action.')
-    }
-}
+  switch (action.type) {
+    case "set_msg":
+      return {
+        ...store,
+        msg: action.payload as string,
+      };
+    default:
+      throw Error("Unknow action.");
+  }
+};
