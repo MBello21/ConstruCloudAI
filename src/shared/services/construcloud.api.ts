24490971701d92c15365c42cae3f408451c0706f
Miguel Ángel García Bello <magarbello@gmail.com>
2026-08-24 13:23:00 +0200

    variable de entorno back

--- src/shared/services/construcloud.api.ts
+++ src/shared/services/construcloud.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const construcloudAPI = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
 });
 
